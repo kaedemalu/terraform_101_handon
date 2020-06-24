78f3f0c0951cf5e6a72e7ef8ee18b5415ed8101d
kaedemalu <kaedemalu@gmail.com>
2020-06-24 09:48:13 +0900

    modify lb

--- http_lb.tf
+++ http_lb.tf
@@ -20,6 +20,22 @@ resource "google_compute_backend_service" "web_backend" {
   health_checks = [google_compute_health_check.web_health.self_link]
 }
 
+resource "google_compute_url_map" "url_map" {
+  name        = "url-map"
+  default_service = google_compute_backend_service.web_backend.self_link
+}
+
+resource "google_compute_target_http_proxy" "http_proxy" {
+  name             = "http_proxy"
+  url_map          = google_compute_url_map.url_map.self_link
+}
+
+resource "google_compute_global_forwarding_rule" "forwarding_rule" {
+  name       = "forwarding_rule"
+  target     = google_compute_target_http_proxy.http_proxy.self_link
+  port_range = "80"
+}
+
 resource "google_compute_instance_group" "web_instance_group" {
   name        = "web-instance-group"
 
