6956d34e558b9ff68fc51eed1ef2bf2c40edfc38
kaedemalu <kaedemalu@gmail.com>
2020-06-24 09:29:13 +0900

    add LB

---
+++ http_lb.tf
@@ -0,0 +1,29 @@
+resource "google_compute_health_check" "web_health" {
+  name = "web-health"
+  timeout_sec        = 1
+  check_interval_sec = 1
+  tcp_health_check {
+    port = "80"
+  }
+}
+
+resource "google_compute_backend_service" "web_backend" {
+  name        = "web-backend"
+  port_name   = "http"
+  protocol    = "HTTP"
+  timeout_sec = 3000
+
+  backend {
+    group = google_compute_instance_group.instance_group.self_link
+  }
+
+  health_checks = [google_compute_health_check.health-check.self_link]
+}
+
+resource "google_compute_instance_group" "web_instance_group" {
+  name        = "web-instance-group"
+
+  instances = [google_compute_instance.web_instance.self_link]
+
+  zone = "asia-northeast1-a"
+}
